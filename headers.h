60d415ff4e575b523f173ef55333578131dcb549
Ailson F. dos Santos <ailsonfds94@gmail.com>
2021-03-10 09:48:37 -0300

    Add headers.h

---
+++ headers.h
@@ -0,0 +1,47 @@
+#ifndef __HEADERS_H__
+#define __HEADERS_H__
+
+#include "tokens.h"
+
+void Prog (void);
+void Pack (void);
+void Impt (void);
+void Impts (void);
+void ProgBody (void);
+void ConstDefPart (void);
+void ConstDef (void);
+void ConstDefs (void);
+void Const (void);
+void SignedConst (void);
+void Num (void);
+void Sign (void);
+void ClassDefPart (void);
+void ClassDef (void);
+void ClassInherance (void);
+void ClassDefs (void);
+void AttrDeclPart (void);
+void AttrInit (void);
+void AttrInits (void);
+void AttrDecl (void);
+void AttrVal (void);
+void AttrDecls (void);
+void MethDeclPart (void);
+void MethHead (void);
+void MethType (void);
+void MethHeads (void);
+void ParSec (void);
+void Param (void);
+void Params (void);
+void ParSecs (void);
+void VarDeclPart (void);
+void VarKeywd (void);
+void VarDef (void);
+void Var (void);
+void VarList (void);
+void VarAssign (void);
+void VarDefs (void);
+void MethDefPart (void);
+void MethDef (void);
+void MethDefs (void);
+
+#endif /** __HEADERS_H__ **/
